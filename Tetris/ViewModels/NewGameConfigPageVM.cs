5aab7854095de33e290ecb7d3f1cba656b151605
GCS08 <shaysol1233@gmail.com>
2025-10-30 09:42:57 +0200

    yes

--- Tetris/ViewModels/NewGameConfigPageVM.cs
+++ Tetris/ViewModels/NewGameConfigPageVM.cs
@@ -8,9 +8,32 @@ using Tetris.Models;
 
 namespace Tetris.ViewModels
 {
-    public class NewGameConfigPageVM
+    internal class NewGameConfigPageVM : ObservableObject
     {
         public ICommand NavBackHomeCommand => new Command(NavHome);
+        public ICommand PickerFocusedCommand => new Command(PickerFocused);
+        public ICommand PickerUnfocusedCommand => new Command(PickerUnfocused);
+        private Color pickerTitleColor = Colors.White;
+        public Color PickerTitleColor
+        {
+            get => pickerTitleColor;
+            set
+            {
+                if (pickerTitleColor != value)
+                {
+                    pickerTitleColor = value;
+                    OnPropertyChanged(nameof(PickerTitleColor));
+                }
+            }
+        }
+        private void PickerFocused()
+        {
+            PickerTitleColor = Colors.Red;
+        }
+        private void PickerUnfocused()
+        {
+            PickerTitleColor = Colors.White;
+        }
         private async void NavHome()
         {
             await Shell.Current.GoToAsync(TechnicalConsts.RedirectMainPageRefresh);
