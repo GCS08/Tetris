2fbaa69e0a43d69afe90071b08c635ea9467b8f0
Shay Solomon <shaysol1233@gmail.com>
2026-03-04 05:28:38 +0200

    some finales. tomorrow go through the more than one return per method.

--- Tetris/Converters/StringAndColorConverter.cs
+++ Tetris/Converters/StringAndColorConverter.cs
@@ -8,28 +8,33 @@ namespace Tetris.Converters
     {
         public static Color ColorNameToColor(string input)
         {
-            if (input == Keys.RedKey) return Colors.Red;
-            if (input == Keys.OrangeKey) return Colors.Orange;
-            if (input == Keys.YellowKey) return Colors.Yellow;
-            if (input == Keys.GreenKey) return Colors.Green;
-            if (input == Keys.BlueKey) return Colors.Blue;
-            if (input == Keys.IndigoKey) return Colors.Indigo;
-            if (input == Keys.VioletKey) return Colors.Violet;
-            if (input == Keys.LightBlueKey) return Colors.LightBlue;
-            return Colors.Transparent;
+            Color color = input switch
+            {
+                Keys.RedKey => Colors.Red,
+                Keys.OrangeKey => Colors.Orange,
+                Keys.YellowKey => Colors.Yellow,
+                Keys.GreenKey => Colors.Green,
+                Keys.BlueKey => Colors.Blue,
+                Keys.IndigoKey => Colors.Indigo,
+                Keys.VioletKey=> Colors.Violet,
+                Keys.LightBlueKey => Colors.LightBlue,
+                _ => Colors.Transparent
+            };
+            return color;
         }
 
         public static string ColorToColorName(Color color)
         {
-            if (color == Colors.Red) return Keys.RedKey;
-            if (color == Colors.Orange) return Keys.OrangeKey;
-            if (color == Colors.Yellow) return Keys.YellowKey;
-            if (color == Colors.Green) return Keys.GreenKey;
-            if (color == Colors.Blue) return Keys.BlueKey;
-            if (color == Colors.Indigo) return Keys.IndigoKey;
-            if (color == Colors.Violet) return Keys.VioletKey;
-            if (color == Colors.LightBlue) return Keys.LightBlueKey;
-            return Keys.TransparentKey;
+            string colorFinal = Keys.TransparentKey;
+            if (color == Colors.Red) colorFinal = Keys.RedKey;
+            else if (color == Colors.Orange) colorFinal = Keys.OrangeKey;
+            else if (color == Colors.Yellow) colorFinal = Keys.YellowKey;
+            else if (color == Colors.Green) colorFinal = Keys.GreenKey;
+            else if (color == Colors.Blue) colorFinal = Keys.BlueKey;
+            else if (color == Colors.Indigo) colorFinal = Keys.IndigoKey;
+            else if (color == Colors.Violet) colorFinal = Keys.VioletKey;
+            else if (color == Colors.LightBlue) colorFinal = Keys.LightBlueKey;
+            return colorFinal;
         }
     }
 }
