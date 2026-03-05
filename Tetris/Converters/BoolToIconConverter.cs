cc470d314eadd3ac58d09cde2b49603f3750a651
GCS08 <shaysol1233@gmail.com>
2026-03-05 04:16:55 +0200

    some converter issues

--- Tetris/Converters/BoolToIconConverter.cs
+++ Tetris/Converters/BoolToIconConverter.cs
@@ -3,9 +3,24 @@ using System.Globalization;
 
 namespace Tetris.Converters
 {
+    /// <summary>
+    /// Converts a boolean value to the corresponding visibility icon.
+    /// Used for bindings where a password visibility state determines 
+    /// which icon (visible / hidden) should be displayed in the UI.
+    /// </summary>
     public class BoolToIconConverter : IValueConverter
     {
-
+        #region Public Methods
+        /// <summary>
+        /// Converts a boolean value into the corresponding icon string.
+        /// If the value is true, the "visibility off" icon is returned.
+        /// If the value is false, the "visibility on" icon is returned.
+        /// </summary>
+        /// <param name="value">The boolean value representing the current visibility state.</param>
+        /// <param name="targetType">The type expected by the binding target.</param>
+        /// <param name="parameter">Optional parameter supplied by the binding.</param>
+        /// <param name="culture">The culture information for the conversion.</param>
+        /// <returns>A string representing the icon that should be displayed.</returns>
         public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             string icon = Icons.Visibility_off;
@@ -14,9 +29,19 @@ namespace Tetris.Converters
             return icon;
         }
 
+        /// <summary>
+        /// Converts the icon value back to the original boolean value.
+        /// This converter does not support reverse conversion and always returns null.
+        /// </summary>
+        /// <param name="value">The value from the binding target.</param>
+        /// <param name="targetType">The type expected by the binding source.</param>
+        /// <param name="parameter">Optional parameter supplied by the binding.</param>
+        /// <param name="culture">The culture information for the conversion.</param>
+        /// <returns>Always returns null because reverse conversion is not implemented.</returns>
         public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
         {
             return null;
         }
+        #endregion
     }
 }
--- Tetris/Converters/StringAndColorConverter.cs
+++ Tetris/Converters/StringAndColorConverter.cs
@@ -4,8 +4,19 @@ using Tetris.Models;
 
 namespace Tetris.Converters
 {
-    public class StringAndColorConverter
+    /// <summary>
+    /// Converter that translates between a string color key (stored in the model)
+    /// and a MAUI Color object (used by the UI).
+    /// Enables binding between UI elements and string-based color values.
+    /// </summary>
+    public class StringAndColorConverter : IValueConverter
     {
+        /// <summary>
+        /// Converts a color key string into its corresponding MAUI Color object.
+        /// Used when the UI needs to display a color that is stored as a string key.
+        /// </summary>
+        /// <param name="input">The color key string.</param>
+        /// <returns>The matching MAUI Color. Returns Transparent if the key is unknown.</returns>
         public static Color ColorNameToColor(string input)
         {
             Color color = input switch
@@ -16,16 +27,23 @@ namespace Tetris.Converters
                 Keys.GreenKey => Colors.Green,
                 Keys.BlueKey => Colors.Blue,
                 Keys.IndigoKey => Colors.Indigo,
-                Keys.VioletKey=> Colors.Violet,
+                Keys.VioletKey => Colors.Violet,
                 Keys.LightBlueKey => Colors.LightBlue,
                 _ => Colors.Transparent
             };
             return color;
         }
 
+        /// <summary>
+        /// Converts a MAUI Color object into its corresponding string key.
+        /// Used when saving or sending color data that is represented as a string.
+        /// </summary>
+        /// <param name="color">The MAUI Color to convert.</param>
+        /// <returns>The string key representing the color.</returns>
         public static string ColorToColorName(Color color)
         {
             string colorFinal = Keys.TransparentKey;
+
             if (color == Colors.Red) colorFinal = Keys.RedKey;
             else if (color == Colors.Orange) colorFinal = Keys.OrangeKey;
             else if (color == Colors.Yellow) colorFinal = Keys.YellowKey;
@@ -34,7 +52,36 @@ namespace Tetris.Converters
             else if (color == Colors.Indigo) colorFinal = Keys.IndigoKey;
             else if (color == Colors.Violet) colorFinal = Keys.VioletKey;
             else if (color == Colors.LightBlue) colorFinal = Keys.LightBlueKey;
+
             return colorFinal;
+        }
+
+        /// <summary>
+        /// Converts a string color key from the binding source into a MAUI Color
+        /// for use in the UI.
+        /// </summary>
+        /// <param name="value">The color key string.</param>
+        /// <param name="targetType">The type expected by the binding target.</param>
+        /// <param name="parameter">Optional binding parameter.</param>
+        /// <param name="culture">Culture information.</param>
+        /// <returns>The corresponding MAUI Color.</returns>
+        public object? Convert(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return ColorNameToColor((string)value);
+        }
+
+        /// <summary>
+        /// Converts a MAUI Color from the UI back into its string key representation.
+        /// Used when updating the underlying data source.
+        /// </summary>
+        /// <param name="value">The MAUI Color value.</param>
+        /// <param name="targetType">The type expected by the binding source.</param>
+        /// <param name="parameter">Optional binding parameter.</param>
+        /// <param name="culture">Culture information.</param>
+        /// <returns>The string key representing the color.</returns>
+        public object? ConvertBack(object? value, Type targetType, object? parameter, CultureInfo culture)
+        {
+            return ColorToColorName((Color)value);
         }
     }
 }
